a5c6a275a09346869076ff75ac9c26e78875b785
Shweta Mathanker <135358941+Shweta-Mathanker@users.noreply.github.com>
2024-09-09 20:18:48 +0530

    Add files via upload

---
+++ 8.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("number do:");
+    scanf("%i",&n);
+    if(n>=0){
+        printf("positive");
+    }
+    else{
+        printf("negetive");
+    }
+    return 0;
+}
